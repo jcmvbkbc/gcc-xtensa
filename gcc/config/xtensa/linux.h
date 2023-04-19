b66c026af230d6bfe2ee05e3b03a5ba51b39e072
Max Filippov <jcmvbkbc@gmail.com>
2023-04-19 02:49:12 -0700

    fix FDPIC

--- gcc/config/xtensa/linux.h
+++ gcc/config/xtensa/linux.h
@@ -64,6 +64,10 @@ along with GCC; see the file COPYING3.  If not see
   %{mabi=windowed:--abi-windowed} \
   %{mabi=call0:--abi-call0}"
 
+#undef	STARTFILE_SPEC
+#define STARTFILE_SPEC "%{mfdpic:%{!shared:crtreloc.o%s}} " \
+  GNU_USER_TARGET_STARTFILE_SPEC
+
 #undef LOCAL_LABEL_PREFIX
 #define LOCAL_LABEL_PREFIX	"."
 
